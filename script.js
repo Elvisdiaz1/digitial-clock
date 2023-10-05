a6c8328436e91db6e6c5834b2158b5e984572ed0
Elvis Diaz <elvisdiaz020@gmail.com>
2023-10-05 15:24:35 -0400

    adds button to change the color of the clock

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-let timeContainer = document.createElement("p");
-timeContainer.classList.add("time");
-document.body.appendChild(timeContainer);
+let time = document.createElement("p");
+time.id = "time";
+document.getElementById("timeContainer").appendChild(time);
+
+let colorButton = document.createElement("button");
+colorButton.innerText = "Change Clock Color";
+document.getElementById("buttons").appendChild(colorButton);
 
 setInterval(function () {
-  timeContainer.innerText = dayjs().format("hh-mm-ss a");
+  time.innerText = dayjs().format("hh-mm-ss a");
 }, 100);
+
+// window.addEventListener("click", changeColor);
+function changeColor() {
+  document.getElementById("time").style.color =
+    document.getElementById("MyColorPicker").value;
+}
+
+colorButton.addEventListener("click", function () {
+  document.getElementById("MyColorPicker").style.display = "block";
+  colorButton.innerText = "Confirm";
+  changeColor();
+});
